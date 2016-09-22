e44b703f4b37dfb3217d92ac2305d1d4e0d12dfa
Sean Buckingham <Sean Buckingham>
2016-09-22 01:02:26 -0400

    this is the main application. equivalent to 'MainFXApplication' in the M3 homework. Is the base of how everything runs in the application

--- src/fxapp/MainApplication.java
+++ src/fxapp/MainApplication.java
@@ -1,7 +1,9 @@
 package fxapp;
 
+import controller.LoginScreenController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -11,23 +13,23 @@ import controller.WelcomeScreenController;
 
 public class MainApplication extends Application {
     /** the main container for the application window */
-    private Stage welcomeScreen;
+    private Stage mainAppScreen;
 
     /** the main layout for the main window */
     private BorderPane rootLayout;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        welcomeScreen = primaryStage;
-        initRootLayout(welcomeScreen);
+        mainAppScreen = primaryStage;
+        initRootLayout(mainAppScreen);
     }
 
     /**
      * Initialize the main screen for the application.  Most other views will be shown in this screen.
      *
-     * @param welcomeScreen  the main Stage window of the application
+     * @param mainAppScreen  the main Stage window of the application
      */
-    private void initRootLayout(Stage welcomeScreen) {
+    private void initRootLayout(Stage mainAppScreen) {
         try {
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
@@ -40,13 +42,42 @@ public class MainApplication extends Application {
 
             // Show the scene containing the root layout.
             Scene scene = new Scene(rootLayout);
-            welcomeScreen.setTitle("Clean Water Application");
-            welcomeScreen.setScene(scene);
-            welcomeScreen.show();
+            mainAppScreen.setTitle("Clean Water Application");
+            mainAppScreen.setScene(scene);
+            mainAppScreen.show();
 
         } catch (IOException e){
             //error on load, so log it
             System.out.println("Failed to find the fxml file for MainScreen!!");
+            e.printStackTrace();
+        }
+
+    }
+
+    public void switchToLoginScreen() {
+        try {
+            // Pointing loader to login screen fxml file
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApplication.class.getResource("../view/LoginScreen.fxml"));
+            rootLayout = loader.load();
+
+            // Give the controller access to the main app.
+            LoginScreenController ctrl = loader.getController();
+            ctrl.setMainApp(this);
+
+            /** Creating a new scene to display the login screen
+             *  Ensures that the content of the existing window is changed
+             *  and another window is not created
+             */
+            Scene scene = new Scene(rootLayout);
+            mainAppScreen.setScene(scene);
+            mainAppScreen.show();
+
+
+
+        } catch (IOException e){
+            //error on load, so log it
+            System.out.println("Failed to find the fxml file for Log in Screen!!");
             e.printStackTrace();
         }
 
