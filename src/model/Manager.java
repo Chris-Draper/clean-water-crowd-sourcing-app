70ff8f805a25258618185449dccaab8cc14734fe
Sean Buckingham <Sean Buckingham>
2016-09-22 01:06:36 -0400

    Since we did not have a model folder or any of the java classes needed i went ahead and made them. The UserType java file is an Enum file which i needed for my combo box. OverallUser is an abstract java class which all other user types are children of. I figured it'd be best if we have to make general methods to just add them their so they all inherit instead of copy pasting a bunch.

---
+++ src/model/Manager.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * Created by sbuck on 9/21/2016.
+ */
+public class Manager extends OverallUser {
+
+    public Manager(String username, String password) {
+        super(username, password);
+    }
+}
