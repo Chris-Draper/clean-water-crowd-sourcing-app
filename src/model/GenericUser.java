39de115d04dbfb2dcf1ee17887d8d2a9a6221f80
Sean Buckingham <Sean Buckingham>
2016-09-27 22:28:59 -0400

    Re-extended certain classes to maximize polymorphism. The current class inheritance is as follows:
       	GenericUser --> User --> Worker --> Manager
    	     |
    	     ---------> Administrator
    
    	   *All classes extend each other starting from User except the
    	   Administrator class. Since it cant do what the User does it
    	   only extends the GenericUser class
    
    Renamed the "OverallUser" class to "GenericUser" for clarity
    
    Also added blank dummy methods that will be implemented later. These
    methods detail what each User Type will be able to do in the future. Ps:
    if you think they can be named better rename them.
    
    Also added "date" local variable to the GenericUser class which is
    automatically filled with that day's date and time of which the User object was
    created.

---
+++ src/model/GenericUser.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.util.Date;
+
+/**
+ * Created by sbuck on 9/21/2016.
+ */
+public abstract class GenericUser {
+
+    private String username;
+    private String password;
+    private UserType userType;
+    private final Date date;
+
+    public GenericUser(String username, String password, UserType userType) {
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
+        this.date = new Date();
+    }
+
+    public String getUsername() { return username; }
+
+    public String getPassword() { return password; }
+
+    public UserType getUserType() { return userType; }
+
+    public Date getDate() { return date; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public void setPassword(String password) { this.password = password; }
+
+}
